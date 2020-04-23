5321f12dd9cc2673c4f8e80ecd51be31319eb7de
Mentor Gashi <m.gashi@albismart.com>
2020-04-23 12:50:54 +0200

    Adding dialog to Archive button, emitting multiple events

--- src/buttons/Archive.vue
+++ src/buttons/Archive.vue
@@ -1,14 +1,34 @@
 <template>
 	<button type="button"
 		class="button is-outlined is-danger"
-		@click="doClick()"><i class="fal fa-archive"></i></button>
+		@click="doClick()"><i class="fa fa-archive"></i></button>
 </template>
 <script>
 	export default {
-		name: 'Archive',
+		name: "Archive",
+		props: {
+			dialog: {
+				type: Boolean,
+				default: false
+			}
+		},
 		methods: {
 			doClick() {
-				this.$emit('click');
+				this.$emit("click");
+				if(this.dialog) this.confirm();
+			},
+			confirm() {
+				this.$buefy.dialog.confirm({
+					title: "Archive item",
+					message: "You are about to <b>archive</b> an item?<br/>Archiving will unlist item from references.",
+					confirmText: "Archive",
+					type: "is-danger",
+					hasIcon: true,
+					icon: "archive fa-2x",
+					iconPack: "fa",
+					onConfirm: () =>  { this.$emit("archived") },
+					onCancel: () =>  { this.$emit("canceled") }
+				});
 			}
 		}
 	}
