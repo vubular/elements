a09ec0e6316425f9a01468b72da807b55d65bda6
Mentor Gashi <m.gashi@albismart.com>
2020-07-10 11:31:51 +0200

    Minor fixes

--- src/buttons/Archive.vue
+++ src/buttons/Archive.vue
@@ -2,7 +2,7 @@
 	<button type="button"
 		class="button is-danger"
 		:class="{'is-outlined': dialog}"
-		@click="doClick()"><slot><i class="fa fa-archive"></i></slot></button>
+		@click="doClick()"><slot><i :class="icon"></i></slot></button>
 </template>
 <script>
 	export default {
@@ -19,6 +19,14 @@
 			message: {
 				type: String,
 				default: "You are about to <b>archive</b> an item?<br/>Archiving will unlist item from references."
+			},
+			button: {
+				type: String,
+				default: "Archive"
+			},
+			icon: {
+				type: String,
+				default: "fa fa-archive"
 			}
 		},
 		methods: {
@@ -34,10 +42,10 @@
 				this.$buefy.dialog.confirm({
 					title: this.title,
 					message: this.message,
-					confirmText: "Archive",
+					confirmText: this.button,
 					type: "is-danger",
 					hasIcon: true,
-					icon: "archive fa-2x",
+					icon: this.icon.split(" ").pop().replace("fa-","") + " fa-2x",
 					iconPack: "fa",
 					onConfirm: () =>  { this.$emit("archived") },
 					onCancel: () =>  { this.$emit("canceled") }
--- src/statics/Conditional.vue
+++ src/statics/Conditional.vue
@@ -1,17 +1,11 @@
 <template>
-	<div class="is-fullwidth" :class="{'precond': !condition}">
+	<div class="is-fullwidth" :class="{'precond': !condition,'no-border': noBorder}">
 		<slot v-if="condition"></slot>
 		<div v-else class="columns disfocus">
-			<div class="column is-1 has-text-really-centered">
-				<i class="fal fa-divide fa-3x fa-border"></i>
-			</div>
-			<div class="column is-11">
-				<p class="subtitle is-3 has-text-grey" style="font-weight:200;margin:0">
-					Please try filling other requirements until this section reveals.
-				</p>
-				<p class="subtitle is-4 has-text-grey-light" style="font-weight:200;margin:0">
-					Contents here are conditioned by surrounding elements above or below.
-				</p>
+			<div v-if="!silent" class="column is-1 has-text-really-centered"><i :class="icon"></i></div>
+			<div v-if="!silent" class="column is-11">
+				<p class="subtitle is-3 has-text-grey" style="font-weight:200;margin:0" v-html="title"></p>
+				<p class="subtitle is-4 has-text-grey-light" style="font-weight:200;margin:0" v-html="description"></p>
 			</div>
 		</div>
 	</div>
@@ -19,12 +13,30 @@
 <script>
 	export default {
 		name: "Conditional",
-		props: ["condition", "icon", "title", "description", "bordered"]
+		props: {
+			condition: [ Boolean, String, Number, Object, Array ],
+			icon: {
+				type: String,
+				default: "fal fa-divide fa-3x fa-border"
+			},
+			title: {
+				type: String,
+				default: "Please try filling other requirements until this section reveals."
+			},
+			description: {
+				type: String,
+				default: "Contents here are conditioned by surrounding elements above or below."
+			},
+			noBorder: Boolean,
+			silent: Boolean
+		}
 	}
 </script>
 <style scoped>
 	.precond { margin-top: 30px; }
+	.precond.no-border { margin-top: 0; }
 	.precond .columns { border-top: 1px solid #f3f3f3; }
+	.precond.no-border .columns { border: 0; }
 	.precond .columns .column { padding: 30px; }
 	.precond .columns.disfocus { opacity: .5; transition: all .5s ease-in-out; }
 	.precond .columns.disfocus:hover { opacity: 1; }
